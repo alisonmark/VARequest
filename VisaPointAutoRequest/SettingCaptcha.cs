afd1db6fc1022f3e557118902edb22b5ae3d8194
just_Q <quanlv.mr@gmail.com>
2016-09-20 16:28:48 +0000

    add SettingCaptcha

---
+++ VisaPointAutoRequest/SettingCaptcha.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisaPointAutoRequest
+{
+    class SettingCaptcha
+    {        
+        public string account { get; set; }
+        public string password { get; set; }
+
+        public SettingCaptcha(string account, string password)
+        {
+            this.account = account;
+            this.password = password;
+        }
+    }
+}
